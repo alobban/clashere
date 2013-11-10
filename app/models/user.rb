18eae9facc15bfc255b5299a1872a5dd894f688c
Andrew Lobban <andrewglobban@gmail.com>
2013-11-10 06:47:46 -0600

    Make a basic User Model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-	has_many :classified_ads
-
-	validates :first_name, presence: true, length: { minimum: 5, maximum: 20 }
-	validates :last_name, presence: true, length: { minimum: 5, maximum: 20 }
-	validates :email, presence: true, length: { maximum: 40 }, uniqueness: true
-	validates :phone, presence: true
-	validates :password, presence: true, length: {minimum: 6}
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+					  uniqueness: { case_sensitive: false }
+	has_secure_password
+	validates :password, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131110010240) do
+ActiveRecord::Schema.define(version: 20131110124009) do
 
   create_table "classified_ads", force: true do |t|
     t.string   "title"
@@ -41,13 +41,13 @@ ActiveRecord::Schema.define(version: 20131110010240) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
     t.string   "email"
-    t.string   "phone"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
