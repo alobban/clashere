1c99f2e96983bd36c06754c2527e87826d6c40ff
Andrew Lobban <andrewglobban@gmail.com>
2013-11-12 00:04:27 -0600

    Add Sign-In-Out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Clashere::Application.routes.draw do
   
   match '/signup',  to: 'users#new',              via: 'get'
+  match '/signin',  to: 'sessions#new',           via: 'get'
+  match '/signout', to: 'sessions#destroy',       via: 'delete'
   root 'static_pages#home'
   match '/help',    to: 'static_pages#help',      via: 'get'
   match '/about',   to: 'static_pages#about',     via: 'get'
   match '/contact', to: 'static_pages#contact',   via: 'get'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :classified_ads
 
